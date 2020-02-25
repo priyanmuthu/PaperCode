c75cb98a36ec341f0c15a8c2f9391c933f13cd41
Priyan Vaithilingam <priyanmuthu@gmail.com>
2020-02-24 23:35:51 -0800

    table of contents

--- papercode/printer/configurable_base_div.py
+++ papercode/printer/configurable_base_div.py
@@ -19,7 +19,10 @@ class ConfigurableBaseDiv(BaseDiv):
         self.RemoveEmptyLines = True
 
         # Inner Working
-        # node_parition_dict = {}
+        self.node_parition_dict = {}
+        self.function_nodes = []
+        self.node_page_dict = {}
+
 
 
     def generate_html(self, soup: BeautifulSoup, paper: Paper):
@@ -41,9 +44,14 @@ class ConfigurableBaseDiv(BaseDiv):
         partitions = self.getPartitions(root_node)
         
         # Post process
+        # Removing empty lines
         if self.RemoveEmptyLines:
             partitions = self.remove_empty_partitions(partitions)
+        # Squashing comments to sidebar
         partitions = self.squash_partitions(partitions)
+        # Table-of-contents
+        self.pre_calculcate_pages(partitions, paper) # Pre-calculating pages
+        partitions = self.table_of_contents(partitions)
 
         # Create partition node dict
         line_count = 0
@@ -57,17 +65,64 @@ class ConfigurableBaseDiv(BaseDiv):
 
             # If the new partition exceeds the page limit and the page gap is not more than half the page
             if (line_count + block_length) > max_lines and (max_lines - line_count) < max_lines/2:
-                # highlight_table.append(self.get_page_break_table_row(soup, max_lines - line_count - 1))
                 highlight_table = self.generate_highlight_table(soup)
                 line_count = 0
             table_row = self.get_table_row_from_partition(soup, p)
             highlight_table.append(table_row)
             line_count += part_length
-            # print('line_count: ', line_count)
-            # print(p['base']['line_nos'])
             line_count = line_count % (max_lines)
-            # Todo: Write code for page counter
-            # If the line count is greater than max_length then increment the page counter and set line count to zero
+
+    def pre_calculcate_pages(self, partitions: list, paper: Paper):
+        # Create partition node dict
+        page_count = 1
+        line_count = 0
+        max_lines = paper.max_lines
+        # self.node_page_dict
+
+        for p in partitions:
+            # base, side bar : create tables seperately for both
+            part_length = p['base']['length']
+            block_length = part_length
+
+            # If the new partition exceeds the page limit and the page gap is not more than half the page
+            if (line_count + block_length) > max_lines and (max_lines - line_count) < max_lines/2:
+                page_count += 1
+                line_count = 0
+            
+            # Debug print
+            line_nos = p['base']['line_nos']
+            print(line_nos[0], '...', line_nos[-1], ' @ ', page_count)
+
+            if p['node'] not in self.node_page_dict:
+                self.node_page_dict[p['node']] = page_count
+
+            line_count += part_length
+            if line_count > max_lines:
+                page_count += int(line_count/max_lines)
+            line_count = line_count % (max_lines)
+    
+    def table_of_contents(self, partitions: list):
+        # iterate over function nodes and append to sidebar
+        for func in self.function_nodes:
+            func_base_part = self.node_parition_dict[func]['base']
+            func_sidebar = self.node_parition_dict[func]['sidebar']
+            call_locs = {}
+            #  Get all function calls from this function and get its location
+            for call in func.function_calls:
+                call_line = call.start_pos.line
+                cfunc = call.function_node
+                cfunc_line = cfunc.start_pos.line
+                cfunc_page = self.node_page_dict[cfunc]
+                call_locs[call_line] = '{0} is defined at page {1}, line {2}'.format(cfunc.name, cfunc_page, cfunc_line)
+                print(call_locs[call_line])
+            # go through the partition and check if the call line already has any stuff in it
+            for i in range(func_base_part['length']):
+                lno = func_base_part['line_nos'][i]
+                sidebar_scl = func_sidebar['source_code_lines'][i]
+                if lno in call_locs and sidebar_scl.strip() == '':
+                    func_sidebar['source_code_lines'][i] = call_locs[lno]
+        return partitions
+
 
     def generate_highlight_table(self, soup: BeautifulSoup):
         html_body = soup.find('body')
@@ -116,55 +171,55 @@ class ConfigurableBaseDiv(BaseDiv):
 
     def get_table_row_from_partition(self, soup: BeautifulSoup, part: dict):
         # Making the base
-            base_part = part['base']
-            part_uuid = uuid.uuid4().hex
-            _, code_base = UtilMethods.get_pre_formated_text(base_part, self.code_file.language)
-            # code_base = code_base.splitlines()[:base_part['length']]
-            # code_base = '\n'.join(code_base)
-            line_str = '<pre>' + '\n'.join(str(lno) for lno in base_part['line_nos']) + '</pre>'
-            
-            # Generating the line no div
-            line_no_div = soup.new_tag('div')
-            line_no_div['class'] = ['linenodiv']
-            line_no_div.append(BeautifulSoup(line_str, 'html.parser'))
-            
-            # Generating the line no td
-            line_table_data = soup.new_tag('td')
-            if part['node'] not in self.elements_line_td:
-                self.elements_line_td[part['node']] = part_uuid + '-line'
-                line_table_data['id'] = self.elements_line_td[part['node']]
-            line_table_data['class'] = ['linenos']
-            line_table_data.append(line_no_div)
+        base_part = part['base']
+        part_uuid = uuid.uuid4().hex
+        _, code_base = UtilMethods.get_pre_formated_text(base_part, self.code_file.language)
+        # code_base = code_base.splitlines()[:base_part['length']]
+        # code_base = '\n'.join(code_base)
+        line_str = '<pre>' + '\n'.join(str(lno) for lno in base_part['line_nos']) + '</pre>'
+        
+        # Generating the line no div
+        line_no_div = soup.new_tag('div')
+        line_no_div['class'] = ['linenodiv']
+        line_no_div.append(BeautifulSoup(line_str, 'html.parser'))
+        
+        # Generating the line no td
+        line_table_data = soup.new_tag('td')
+        if part['node'] not in self.elements_line_td:
+            self.elements_line_td[part['node']] = part_uuid + '-line'
+            line_table_data['id'] = self.elements_line_td[part['node']]
+        line_table_data['class'] = ['linenos']
+        line_table_data.append(line_no_div)
 
-            # Generating the code highlight div
-            highlight_div = soup.new_tag('div')
-            highlight_div['class'] = ['highlight']
-            highlight_div.append(BeautifulSoup(code_base, 'html.parser'))
+        # Generating the code highlight div
+        highlight_div = soup.new_tag('div')
+        highlight_div['class'] = ['highlight']
+        highlight_div.append(BeautifulSoup(code_base, 'html.parser'))
 
-            # Generating the code td
-            code_table_data = soup.new_tag('td')
-            if part['node'] not in self.elements_code_td:
-                self.elements_code_td[part['node']] = part_uuid + '-code'
-                code_table_data['id'] = self.elements_code_td[part['node']]
-            code_table_data['class'] = ['code']
-            code_table_data.append(highlight_div)
+        # Generating the code td
+        code_table_data = soup.new_tag('td')
+        if part['node'] not in self.elements_code_td:
+            self.elements_code_td[part['node']] = part_uuid + '-code'
+            code_table_data['id'] = self.elements_code_td[part['node']]
+        code_table_data['class'] = ['code']
+        code_table_data.append(highlight_div)
 
-            # Generating the side bar table
-            sidebar_table_data = soup.new_tag('td')
-            if part['node'] not in self.elements_sidebar_td:
-                self.elements_sidebar_td[part['node']] = part_uuid + '-sidebar'
-                sidebar_table_data['id'] = self.elements_sidebar_td[part['node']]
-            sidebar_table_data['class'] = ['sidebar']
-            if 'sidebar' in part and part['sidebar']['length'] > 0:
-                side_table = self.get_table_for_sidebar(soup, [part['sidebar']])
-                sidebar_table_data.append(side_table)
+        # Generating the side bar table
+        sidebar_table_data = soup.new_tag('td')
+        if part['node'] not in self.elements_sidebar_td:
+            self.elements_sidebar_td[part['node']] = part_uuid + '-sidebar'
+            sidebar_table_data['id'] = self.elements_sidebar_td[part['node']]
+        sidebar_table_data['class'] = ['sidebar']
+        if 'sidebar' in part and part['sidebar']['length'] > 0:
+            side_table = self.get_table_for_sidebar(soup, [part['sidebar']])
+            sidebar_table_data.append(side_table)
 
-            table_row = soup.new_tag('tr')
-            table_row.append(line_table_data)
-            table_row.append(code_table_data)
-            table_row.append(sidebar_table_data)
+        table_row = soup.new_tag('tr')
+        table_row.append(line_table_data)
+        table_row.append(code_table_data)
+        table_row.append(sidebar_table_data)
 
-            return table_row
+        return table_row
     
     def getPartitions(self, node: Node):
         partitions = []
@@ -190,7 +245,7 @@ class ConfigurableBaseDiv(BaseDiv):
                 partitions.append({'base': part, 'node': node})
                 current_line = end_line
         elif type(node) == ClassNode:
-            # Print evenrything except the children
+            # Print everything except the children
             current_line = node.start_pos.line - 1
             end_line = min(node.end_pos.line, len(self.code_file.all_lines))
             children = node.children
@@ -226,10 +281,13 @@ class ConfigurableBaseDiv(BaseDiv):
             partitions.append({'base': part, 'node': node})
         elif type(node) == FunctionNode:
             if self.PushCommentsInFunction:
-                partitions.append(self.getFunctionPartition(node))
+                 function_partition = self.getFunctionPartition(node)
             else:
                 part = self.code_file.get_partition(node.start_pos.line, node.end_pos.line, FunctionNode)
-                partitions.append({'base': part, 'node': node})
+                function_partition = {'base': part, 'node': node}
+            partitions.append(function_partition)
+            self.node_parition_dict[node] = function_partition
+            self.function_nodes.append(node)
         elif type(node) == CallNode:
             pass
         elif type(node) == CommentNode:
@@ -276,6 +334,12 @@ class ConfigurableBaseDiv(BaseDiv):
             source_code_lines.extend(part['source_code_lines'])
             current_line = end_line
         
+        # Matching sidebar to the function length
+        remaining_lines = len(line_nos) - len(sidebar_line_nos)
+        if remaining_lines > 0:
+            sidebar_line_nos.extend([' ' for i in range(remaining_lines)])
+            sidebar_code.extend([' ' for i in range(remaining_lines)])
+
         sidebar_part = {
             'line_nos': sidebar_line_nos, 
             'source_code_lines': sidebar_code, 
@@ -322,7 +386,8 @@ class ConfigurableBaseDiv(BaseDiv):
         for part in partitions:
             if 'base' in part:
                 if sidebar != {}:
-                    if sidebar['sidebar']['length'] > part['base']['length']:
+                    if sidebar['sidebar']['length'] > part['base']['length'] or 'sidebar' in part:
+                        # Todo: Can do better - if there is side bar, but we have initial space
                         res_partition.append({'base': sidebar['sidebar'], 'node': sidebar['node']})
                         sidebar = {}
                     else:
@@ -353,7 +418,7 @@ class ConfigurableBaseDiv(BaseDiv):
             line_no_div.append(BeautifulSoup(line_str, 'html.parser'))
 
             line_table_data = soup.new_tag('td')
-            line_table_data['class'] = ['linenos']
+            line_table_data['class'] = ['sidelinenos']
             line_table_data.append(line_no_div)
 
 
